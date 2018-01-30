193f4b941dcb4dad54f6ac2bbe3cf66d32c0f6da
lidianvilazio <lidianvilazio@hotmail.com>
2018-01-30 10:32:19 -0500

    Badges and Schedules

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,43 @@
+require 'pry'
 # Write your code here.
+
+
+def badge_maker(name)
+    return "Hello, my name is #{name}."
+end
+
+
+def batch_badge_creator(array)
+    ar = []
+    array.each do |e|
+        ar << badge_maker(e)
+    end
+    ar
+end
+
+
+def assign_rooms(array)
+    answer = []
+    i = 0
+    while i < array.length
+        answer << "Hello, #{array[i]}! You'll be assigned to room #{i+1}!"
+        i += 1
+    end
+    answer
+end
+
+
+def printer(array)
+    arr = batch_badge_creator(array)
+    ar = assign_rooms(array)
+    arr.each do |e|
+        puts e
+    end
+    ar.each do |e|
+        puts e
+    end
+#    binding.pry
+end
+
+#printer(["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"])
+
